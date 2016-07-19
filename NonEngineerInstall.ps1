da434893f927be83ded559f3db1bf1b127180070
Matt <matt.sonnhalter@gmail.com>
2016-07-19 15:25:39 -0500

    Added initial scripts

---
+++ NonEngineerInstall.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+# Chocolatey
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# install applications
+cinst googlechrome
+cinst slack
+cinst 7zip
+cinst zoom
+cinst adobereader
